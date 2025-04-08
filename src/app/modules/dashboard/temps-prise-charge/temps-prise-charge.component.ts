d305e2a2b46dac426a1dee478ba211ab3aa61d2e
saidsaoudi <said.iduoas@gmail.com>
2025-04-08 11:32:35 +0100

    Temps de prise en charge

--- src/app/modules/dashboard/temps-prise-charge/temps-prise-charge.component.ts
+++ src/app/modules/dashboard/temps-prise-charge/temps-prise-charge.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface Lane {
+  name: string;
+  blocks: ('good' | 'warning' | 'danger')[];
+}
 @Component({
   selector: 'app-temps-prise-charge',
   standalone:false,
@@ -7,5 +11,33 @@ import { Component } from '@angular/core';
   styleUrl: './temps-prise-charge.component.scss'
 })
 export class TempsPriseChargeComponent {
+
+  lanes: Lane[] = [
+    {
+      name: 'Voie 1',
+      blocks: ['good', 'warning', 'good', 'danger']
+    },
+    {
+      name: 'Voie 2',
+      blocks: ['danger', 'good', 'good', 'warning']
+    },
+    {
+      name: 'Voie 3',
+      blocks: ['good', 'danger', 'good', 'good']
+    }
+  ];
+
+  getStatusColor(status: string): string {
+    switch (status) {
+      case 'good':
+        return 'bg-emerald-400';
+      case 'warning':
+        return 'bg-orange-400';
+      case 'danger':
+        return 'bg-red-500';
+      default:
+        return 'bg-gray-400';
+    }
+  }
 
 }
